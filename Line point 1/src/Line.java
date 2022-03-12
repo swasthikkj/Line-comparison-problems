72ced5065e2cea50a1fc36918f9d5e8030f08e2a
swasthikkj <99305988+swasthikkj@users.noreply.github.com>
2022-03-12 17:55:14 +0530

    program

---
+++ Line point 1/src/Line.java
@@ -0,0 +1,17 @@
+
+public class Line {
+	public static void main( String[] args )
+{
+			    System.out.println("Welcome to Line Comparsion program");
+
+		    	int x1;
+		    	int x2;
+		    	int y1;
+		    	int y2;
+				int  line1;
+				x1 = 3; y1 = 4;
+				x2 = 2; y2 = 5;
+				line1 = (int)Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+				System.out.print(line1);
+		    }
+		} 
